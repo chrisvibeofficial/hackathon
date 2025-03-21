bb1eb083ad04733fa94da915b9d21a5b58496977
chrisvibeofficial <jueffizzy@gmail.com>
2025-03-22 00:14:00 +0100

    updated file

--- routes/plan.js
+++ routes/plan.js
@@ -1,11 +1,11 @@
-const { createPlan, getPlans, getPlan, updatePlan, deletePlan } = require('../controllers/plan');
+const { createPlan, getAllPlans, getPlan, updatePlan, deletePlan } = require('../controllers/plan');
 const { authorize, authenticate } = require('../middlewares/authorization');
 
 const router = require('express').Router();
 
 router.post('/create/plan', authorize, createPlan);
-router.get('/subcription/plans', authenticate, getPlans);
-router.get('/subscription/plan/:planId', authenticate, getPlan);
+router.get('/all/plan', authenticate, getAllPlans);
+router.get('/plan/:planId', authenticate, getPlan);
 router.put('/update/plan/:planId', authorize, updatePlan);
 router.delete('/delete/plan/:planId', authorize, deletePlan);
 
