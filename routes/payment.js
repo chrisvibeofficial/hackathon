45cfd32338bf043501ee22674fdf6a2c77e7bd1d
chrisvibeofficial <jueffizzy@gmail.com>
2025-03-23 23:50:02 +0100

    update

--- routes/payment.js
+++
@@ -1,9 +0,0 @@
-const { initializePayment, verifyPayment } = require('../controllers/payment');
-const { authenticate } = require('../middlewares/authorization');
-
-const router = require('express').Router();
-
-router.get('/initialize/payment/:planId', authenticate, initializePayment);
-router.get('/verify/payment', authenticate, verifyPayment);
-
-module.exports = router;
